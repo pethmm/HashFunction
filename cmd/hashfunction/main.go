09a9fabc9b37a55813892ca4ee5073c953e263fe
Pety <38808074+pethmm@users.noreply.github.com>
2025-10-28 21:13:09 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/hashfunction/main.go
@@ -0,0 +1,23 @@
+// cmd/hashfunction/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "hashfunction/internal/hashfunction"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := hashfunction.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
